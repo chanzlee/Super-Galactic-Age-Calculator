d6de62f824dd7e427327dd8735c316a9994b5acf
Chan Lee <chanethanlee@gmail.com>
2018-10-12 10:52:35 -0700

    add mercury age calculator test.

--- spec/ageCalculator-spec.js
+++ spec/ageCalculator-spec.js
@@ -7,4 +7,10 @@ describe('AgeCalculator', function() {
     let testAge = testBirthday.earthAgeCalculator();
     expect(testAge).toEqual(26);
   });
+
+  it('should test whether mercuryAgeCalculator returns correct mercury Age', function() {
+    let testBirthday = new AgeCalculator("1992-09-27");
+    let testAge = testBirthday.mercuryAgeCalculator();
+    expect(testAge).toEqual(107);
+  });
 });
