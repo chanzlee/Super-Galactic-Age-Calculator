32d4a03685c409672d6f0695366175d7dcf65da0
Chan Lee <chanethanlee@gmail.com>
2018-10-12 13:36:20 -0700

    add life expectancy front-end function.

--- src/main.js
+++ src/main.js
@@ -6,10 +6,18 @@ import { AgeCalculator } from './AgeCalculator.js';
 
 $(document).ready(function() {
   $("#planet-age-result").hide();
-  
+  $("#life-expectancy-result").hide();
+
   $("#planet-age").submit(function(event){
     event.preventDefault();
     let birthDay = $("#birthDay").val();
+    let smoking = $("#smoking").val();
+    let alcohol = $("#alcohol").val();
+    let genetics = $("#genetics").val();
+    let obesity = $("#obesity").val();
+    let relationship = $("#relationship").val();
+    let optimisticity = $("#optimisticity").val();
+
     let ageCalcInstance = new AgeCalculator(birthDay);
     let earthAge = ageCalcInstance.earthAgeCalculator();
     let mercuryAge = ageCalcInstance.mercuryAgeCalculator();
@@ -17,11 +25,25 @@ $(document).ready(function() {
     let marsAge = ageCalcInstance.marsAgeCalculator();
     let jupiterAge = ageCalcInstance.jupiterAgeCalculator();
 
+    let earthLeftLife = ageCalcInstance.getLeftLife("earth" smoking, alcohol, genetics, obesity, relationship);
+    let mercuryLeftLife = ageCalcInstance.getLeftLife("mercury" smoking, alcohol, genetics, obesity, relationship);
+    let venusLeftLife = ageCalcInstance.getLeftLife("venus" smoking, alcohol, genetics, obesity, relationship);
+    let marsLeftLife = ageCalcInstance.getLeftLife("mars" smoking, alcohol, genetics, obesity, relationship);
+    let jupiterLeftLife = ageCalcInstance.getLeftLife("jupiter" smoking, alcohol, genetics, obesity, relationship);
+
     $("#earth").text(`${earthAge}`);
     $("#mercury").text(`${mercuryAge}`);
     $("#venus").text(`${venusAge}`);
     $("#mars").text(`${marsAge}`);
     $("#jupiter").text(`${jupiterAge}`);
+
+    $("#earth p").text(`${earthLeftLife}`);
+    $("#mercury p").text(`${mercuryLeftLife}`);
+    $("#venus p").text(`${venusLeftLife}`);
+    $("#mars p").text(`${marsLeftLife}`);
+    $("#jupiter p").text(`${jupiterLeftLife}`);
+
     $("#planet-age-result").show();
+    $("#life-expectancy-result").show();
   });
 });
