73122e55a0955730e4a78b6e7ff876d6066e247b
Chan Lee <chanethanlee@gmail.com>
2018-10-12 10:21:19 -0700

    add days in year data for planets.

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const mercuryDaysInYear = 87.97;
+export const venusDaysInYear = 224.7;
+export const earthDaysInYear = 365.26;
+export const marsDaysInYear= 1.88 * earthDaysInYear;
+export const jupiterDaysInYear = 11.86 * earthDaysInYear;
+
+export const secondsInYear = 31536000;
