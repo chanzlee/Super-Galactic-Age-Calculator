e5327bf79b1e7c0cd1ee1e481d329c5df7627758
Chan Lee <chanethanlee@gmail.com>
2018-10-12 11:29:30 -0700

    create planet age front-end logic.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { AgeCalculator } from './AgeCalculator.js';
 
 $(document).ready(function() {
+  $("#planet-age-result").hide();
+  
+  $("#planet-age").submit(function(event){
+    event.preventDefault();
+    let birthDay = $("#birthDay").val();
+    let ageCalcInstance = new AgeCalculator(birthDay);
+    let earthAge = ageCalcInstance.earthAgeCalculator();
+    let mercuryAge = ageCalcInstance.mercuryAgeCalculator();
+    let venusAge = ageCalcInstance.venusAgeCalculator();
+    let marsAge = ageCalcInstance.marsAgeCalculator();
+    let jupiterAge = ageCalcInstance.jupiterAgeCalculator();
 
+    $("#earth").text(`${earthAge}`);
+    $("#mercury").text(`${mercuryAge}`);
+    $("#venus").text(`${venusAge}`);
+    $("#mars").text(`${marsAge}`);
+    $("#jupiter").text(`${jupiterAge}`);
+    $("#planet-age-result").show();
+  });
 });
