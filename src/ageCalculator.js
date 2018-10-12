ce2fa94203f9009e4fb316e36a9a257affd2438a
Chan Lee <chanethanlee@gmail.com>
2018-10-12 10:34:47 -0700

    add earth age calculator function.

---
+++ src/ageCalculator.js
@@ -0,0 +1,14 @@
+import * as constants from './constants.js';
+
+
+export class AgeCalculator {
+  constructor(birthDay) {
+    this.birthDay = birthDay;
+  }
+  earthAgeCalculator(){
+    let birthDay = new Date(this.birthDay);
+    let lifeSpan = (Date.now() - birthDay.getTime()) / 1000;
+    let earthAge = Math.floor(lifeSpan / constants.secondsInYear);
+    return earthAge;
+  }
+}
