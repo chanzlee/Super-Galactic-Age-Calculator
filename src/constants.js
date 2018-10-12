2e039467d87fee575ae5aa1f2bcf2f54777949d3
Chan Lee <chanethanlee@gmail.com>
2018-10-12 10:39:12 -0700

    add ratio constants.

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,11 @@ export const earthDaysInYear = 365.26;
 export const marsDaysInYear= 1.88 * earthDaysInYear;
 export const jupiterDaysInYear = 11.86 * earthDaysInYear;
 
+
+export const mercuryDaysInYear = 87.97/earthDaysInYear;
+export const venusToEarth = 224.7/earthDaysInYear;
+export const earthToEarth = 365.26;
+export const marsToEarth= 1.88;
+export const jupiterToEarth = 11.86;
+
 export const secondsInYear = 31536000;
