ace504023031aca92e4f7581db76a4732fd80314
Chan Lee <chanethanlee@gmail.com>
2018-10-12 10:53:45 -0700

    update constant name.

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
+//Days
 export const mercuryDaysInYear = 87.97;
 export const venusDaysInYear = 224.7;
 export const earthDaysInYear = 365.26;
 export const marsDaysInYear= 1.88 * earthDaysInYear;
 export const jupiterDaysInYear = 11.86 * earthDaysInYear;
 
-
-export const mercuryDaysInYear = 87.97/earthDaysInYear;
-export const venusToEarth = 224.7/earthDaysInYear;
-export const earthToEarth = 365.26;
-export const marsToEarth= 1.88;
+//ratios
+export const mercuryToEarth = 87.97/earthDaysInYear;
+export const marsToEarth = 224.7/earthDaysInYear;
+export const venusToEarth= 1.88;
 export const jupiterToEarth = 11.86;
 
 export const secondsInYear = 31536000;
