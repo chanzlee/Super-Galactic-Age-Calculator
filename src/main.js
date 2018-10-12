288604dfe5e5cae41a3b85a9e04708487ff19311
Chan Lee <chanethanlee@gmail.com>
2018-10-12 14:32:54 -0700

    edit life expectancy front end function.

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { AgeCalculator } from './AgeCalculator.js';
+import { AgeCalculator } from './ageCalculator.js';
+import { lifeExpectancy } from './lifeExpectancy.js';
 
 $(document).ready(function() {
   $("#planet-age-result").hide();
-  $("#life-expectancy-result").hide();
 
   $("#planet-age").submit(function(event){
     event.preventDefault();
     let birthDay = $("#birthDay").val();
-    let smoking = $("#smoking").val();
-    let alcohol = $("#alcohol").val();
-    let genetics = $("#genetics").val();
-    let obesity = $("#obesity").val();
-    let relationship = $("#relationship").val();
-    
+    let smoking = $("input:radio[name=smoking]:checked").val();
+    let alcohol = $("input:radio[name=alcohol]:checked").val();
+    let genetics = $("input:radio[name=genetics]:checked").val();
+    let obesity = $("input:radio[name=obesity]:checked").val();
+    let relationship = $("input:radio[name=relationship]:checked").val();
+
     let ageCalcInstance = new AgeCalculator(birthDay);
     let earthAge = ageCalcInstance.earthAgeCalculator();
     let mercuryAge = ageCalcInstance.mercuryAgeCalculator();
@@ -24,11 +24,12 @@ $(document).ready(function() {
     let marsAge = ageCalcInstance.marsAgeCalculator();
     let jupiterAge = ageCalcInstance.jupiterAgeCalculator();
 
-    let earthLeftLife = ageCalcInstance.getLeftLife("earth", smoking, alcohol, genetics, obesity, relationship);
-    let mercuryLeftLife = ageCalcInstance.getLeftLife("mercury", smoking, alcohol, genetics, obesity, relationship);
-    let venusLeftLife = ageCalcInstance.getLeftLife("venus", smoking, alcohol, genetics, obesity, relationship);
-    let marsLeftLife = ageCalcInstance.getLeftLife("mars", smoking, alcohol, genetics, obesity, relationship);
-    let jupiterLeftLife = ageCalcInstance.getLeftLife("jupiter", smoking, alcohol, genetics, obesity, relationship);
+    let leftLifeInstance = new lifeExpectancy(birthDay);
+    let earthLeftLife = leftLifeInstance.getLeftLife("earth", smoking, alcohol, genetics, obesity, relationship);
+    let mercuryLeftLife = leftLifeInstance.getLeftLife("mercury", smoking, alcohol, genetics, obesity, relationship);
+    let venusLeftLife = leftLifeInstance.getLeftLife("venus", smoking, alcohol, genetics, obesity, relationship);
+    let marsLeftLife = leftLifeInstance.getLeftLife("mars", smoking, alcohol, genetics, obesity, relationship);
+    let jupiterLeftLife = leftLifeInstance.getLeftLife("jupiter", smoking, alcohol, genetics, obesity, relationship);
 
     $("#earth").text(`${earthAge}`);
     $("#mercury").text(`${mercuryAge}`);
@@ -36,13 +37,12 @@ $(document).ready(function() {
     $("#mars").text(`${marsAge}`);
     $("#jupiter").text(`${jupiterAge}`);
 
-    $("#earth p").text(`${earthLeftLife}`);
-    $("#mercury p").text(`${mercuryLeftLife}`);
-    $("#venus p").text(`${venusLeftLife}`);
-    $("#mars p").text(`${marsLeftLife}`);
-    $("#jupiter p").text(`${jupiterLeftLife}`);
+    $("#earth-expect").text(`${earthLeftLife}`);
+    $("#mercury-expect").text(`${mercuryLeftLife}`);
+    $("#venus-expect").text(`${venusLeftLife}`);
+    $("#mars-expect").text(`${marsLeftLife}`);
+    $("#jupiter-expect").text(`${jupiterLeftLife}`);
 
     $("#planet-age-result").show();
-    $("#life-expectancy-result").show();
   });
 });
