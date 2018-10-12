f37a05bf27e47bc7371a57f4f621800ee72735c6
Chan Lee <chanethanlee@gmail.com>
2018-10-12 11:13:25 -0700

    fix mars venus ratio typo.

--- src/constants.js
+++ src/constants.js
@@ -7,8 +7,8 @@ export const jupiterDaysInYear = 11.86 * earthDaysInYear;
 
 //ratios
 export const mercuryToEarth = 87.97/earthDaysInYear;
-export const marsToEarth = 224.7/earthDaysInYear;
-export const venusToEarth= 1.88;
+export const venusToEarth = 224.7/earthDaysInYear;
+export const marsToEarth= 1.88;
 export const jupiterToEarth = 11.86;
 
 export const secondsInYear = 31536000;
